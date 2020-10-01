baf3d7beb9e7a9b619fa649e5816b8aca2c39e57
tushar14041 <33358349+tushar14041@users.noreply.github.com>
2020-10-02 01:53:06 +0530

    Build project is added after failing in uploadi...
    Build project is added after failing in uploading the code files without build.

---
+++ build/precache-manifest.6432950d646871dcfcee82132e9fb7f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983ca1c9d716959eb50f35212479f44a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6279f59d246bd6aafee9",
+    "url": "/static/css/main.3dcedcdd.chunk.css"
+  },
+  {
+    "revision": "0a0bb93705fba5b56ade",
+    "url": "/static/js/2.5a2a0cb9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5a2a0cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6279f59d246bd6aafee9",
+    "url": "/static/js/main.1feffc48.chunk.js"
+  },
+  {
+    "revision": "5563e3ed2be6fc80024c",
+    "url": "/static/js/runtime-main.31ce5a4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
